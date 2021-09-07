e5d518f131e1737225d675e00d3b936388968cb1
Rajesh Gauswami <rajesh.gauswami@spacecode.com>
2021-09-07 19:52:01 +0530

    Load Testing, Added and Tested with artillery

--- index.js
+++ index.js
@@ -6,12 +6,25 @@ const logger = require('./src/utils/logger');
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
-const app = require('./src/app')(db);
-
+const cluster = require('cluster');
 const buildSchemas = require('./src/utils/databaseWrapper').buildSchemas;
 
-db.serialize(() => {
-  buildSchemas(db);
-  app.listen(port, () =>
-    logger.info(`App started and listening on port ${port}`));
-});
+if (cluster.isMaster) {
+  // Count the machine's CPUs
+  const cpuCount = require('os').cpus().length;
+  // Create a worker for each CPU
+  for (let i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
+  cluster.on('exit', function() {
+    // Replace the dying worker
+    cluster.fork();
+  });
+} else {
+  const app = require('./src/app')(db);
+  db.serialize(() => {
+    buildSchemas(db);
+    app.listen(port, () =>
+      logger.info(`App started and listening on port ${port}`));
+  });
+}
